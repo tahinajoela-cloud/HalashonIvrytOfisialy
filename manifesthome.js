08ce1d1f4bf1ed00e0a25e3714a1b41b123fce1a
Tahina Joela <tahinajoela@gmail.com>
2025-08-17 07:41:12 +0300

    Create manifesthome.js

---
+++ manifesthome.js
@@ -0,0 +1,11 @@
+{
+  "cache_version": "v1.1",
+  "files_to_cache": [
+    "./",
+    "style.css",
+    "script.js",
+    "images/logo.png",
+    "data/config.json"
+    // Ampiana ireo rakitra hafa rehetra
+  ]
+}
